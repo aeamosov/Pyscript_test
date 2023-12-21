a119b951caafc31609d8f73d0dbd8aec71d46c25
aeamosov <a.e.amosov@gmail.com>
2023-12-21 19:45:53 +0300

    Update hh_parcer.py

--- hh_parcer.py
+++ hh_parcer.py
@@ -4,6 +4,7 @@ import requests
 import tqdm 
 import pandas as pd
 import matplotlib.pyplot as plt
+from pyodide.http import pyfetch, FetchResponse
 #Pyscript
 from pyscript import document
 from pyscript import display
@@ -11,12 +12,14 @@ from pyscript import display
 text=str(document.querySelector("#vacancy_name"))
 def get_vacancies(event):
 	target_text='https://api.hh.ru/vacancies?text='+text
-	r = requests.get(target_text).json()
+	response = await pyfetch(url, method="GET", headers=headers)
+	r = await response.json()
+	print(r)
 	p=r['pages'] #Кол-во страниц выдачи
 	vac = []
 	#print('Ожидайте, поиск займет до '+str(p*2)+' секунд')
 	for i in tqdm(range(0, p)):
-		vac.append(requests.get(target_text, params={'page': i, 'per_page':20}).json())
+		vac.append(await pyfetch(url, method="GET", headers=headers, params={'page': i, 'per_page':20}).json())
 	#Выгрузка вакансий
 	vac_row=[]
 	for i in range(0,p):
@@ -25,3 +28,7 @@ def get_vacancies(event):
 			vac_row.append(vac[i]['items'][j])
 	df=pd.DataFrame.from_dict(vac_row, orient='columns')
 	print('Вакансий найдено:',len(df.name))
+
+
+
+
