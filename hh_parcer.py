ad7be540add485eb4135db6e1551741aecae4379
aeamosov <a.e.amosov@gmail.com>
2023-12-21 21:55:31 +0300

    Update hh_parcer.py

--- hh_parcer.py
+++ hh_parcer.py
@@ -12,14 +12,14 @@ from pyscript import display
 text=str(document.querySelector("#vacancy_name"))
 def get_vacancies(event):
 	target_url='https://api.hh.ru/vacancies?text='+text
-	response = pyodide.open_url(target_url)
+	response = pyodide.http.open_url(target_url)
 	r =response.json()
 	print(r)
 	p=r['pages'] #Кол-во страниц выдачи
 	vac = []
 	#print('Ожидайте, поиск займет до '+str(p*2)+' секунд')
 	for i in tqdm(range(0, p)):
-		v=target_url+'?page='+i+'?per_page=20'
+		v=pyodide.http.open_url(target_url+'?page='+i+'?per_page=20')
 		vac.append(v.json())
 	#Выгрузка вакансий
 	vac_row=[]
